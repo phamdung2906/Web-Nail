05aa4e7b7e037c2eec62ce5cd56c29c6bf91157b
phamdung2906 <eualmercury1@gmail.com>
2023-06-23 08:40:17 +0700

    a

--- src/BackendURL.js
+++ src/BackendURL.js
@@ -1,3 +1,3 @@
-export const URLBE =  'https://backenddiamondnail.netlify.app/'
+export const URLBE = 'https://web-nail-backend.vercel.app/' 
 
-// ' 'http://127.0.0.1:5000/'
+// ' 'https://backenddiamondnail.netlify.app/'
